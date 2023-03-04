f79a90e9a3558df13cc071a6d6735c085696b970
Paul Savagner <112151515+PaulSavagner@users.noreply.github.com>
2023-03-04 17:12:20 -0500

    Improve add-cool-link shell script

--- add-cool-link-md-file.sh
+++ add-cool-link-md-file.sh
@@ -1,16 +1,17 @@
 #! bin/usr/sh
 
-echo "Script: what is your name of the new md file?"
+echo "Script: what is your name of the new file?"
 read fileName
+echo "Script: what is extension of the new file?"
+read extension
 cd ~/repos/cool-links
-touch $fileName.md 
-echo [Go back](README.md) >> ~/repos/cool-links/$fileName.md
-# while true; 
-#    read -p "Do you want to add link to new page in homepage? " yn
-#    case $yn in
-#        [Yy]* ) make install; break;;
-#        [Nn]* ) exit;;
-#        * ) echo "Please answer yes or no.";;
-#    esac
-#done
+touch $fileName.$extension 
+echo "[Go back](README.md)" >> ~/repos/cool-links/$fileName.$extension
+echo "Add link in homepage?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) echo "\n[$fileName]($fileName.$extension)  " >> ~/repos/cool-links/README.md; break;;
+        No ) break;;
+    esac
+done
 echo "Script: $fileName created"
