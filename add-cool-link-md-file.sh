89de20625fb35af226493eaec8401cc798d6ed97
Paul Savagner <112151515+PaulSavagner@users.noreply.github.com>
2023-03-04 16:53:21 -0500

    Modify add-cool-link-md-file.sh

--- add-cool-link-md-file.sh
+++ add-cool-link-md-file.sh
@@ -4,4 +4,13 @@ echo "Script: what is your name of the new md file?"
 read fileName
 cd ~/repos/cool-links
 touch $fileName.md 
+echo [Go back](README.md) >> ~/repos/cool-links/$fileName.md
+# while true; 
+#    read -p "Do you want to add link to new page in homepage? " yn
+#    case $yn in
+#        [Yy]* ) make install; break;;
+#        [Nn]* ) exit;;
+#        * ) echo "Please answer yes or no.";;
+#    esac
+#done
 echo "Script: $fileName created"
